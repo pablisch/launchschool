b290cb71ec80a0cf9a0656af881befefe96aa14a
Pablo <pablito@me.com>
2023-03-02 11:10:55 +0000

    Added default hash values to class

--- good_dog.rb
+++ good_dog.rb
@@ -5,7 +5,21 @@ module Speak
 end
 
 class GoodDog
+  attr_accessor :name
   include Speak  # The Speak module is included in the GoodDog class
+
+  def initialize(details = {}) # takes an array as an argument, the '= {}' sets the default to be an empty hash - see below for default values.
+    defaults = {name: "Dog", colour: "white", breed: "poodle"} # sets default values in case any, or all, hash items are missing.
+    defaults.merge!(details) # this merges defaults with details so that any hash values passed in details will overwrite the defaults.
+    ### NOTE any extra hash values, e.g. 'age' will be ignored but not return any error.
+    @name = defaults[:name]
+    @colour = defaults[:colour]
+    @breed = defaults[:breed]
+  end
+
+  def bark
+    puts "#{name} says 'Woof woof!'"
+  end
 end
 
 class HumanBeing
@@ -13,11 +27,25 @@ class HumanBeing
 end
 
 
-sparky = GoodDog.new
+sparky = GoodDog.new({colour: "brown"})
 sparky.speak("Woof!")
+p sparky
 
-bob = HumanBeing.new
-bob.speak("Hello")
+fido = GoodDog.new({breed: "hound", age: 3})
+p fido
 
-puts GoodDog.ancestors  # shows the ancestors of the GoodDog class e.g. the Speak module
+# bob = HumanBeing.new
+# bob.speak("Hello")
 
+# puts GoodDog.ancestors  # shows the ancestors of the GoodDog class e.g. the Speak module
+
+pooch = GoodDog.new({name: "Pooch"})
+p pooch
+
+dennis = GoodDog.new({occupation: "builder", age: 21, breed: "beagle"})
+p dennis
+
+pooch.bark
+
+sparky.name = "Sparky"
+p sparky
